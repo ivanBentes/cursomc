b97e4fa2f3510803ebf4bb25efb15e51e9382f8a
Ivan Bentes <ivan.ra.bentes@gmail.com>
2018-05-15 10:50:16 -0400

    Projeto criado

---
+++ src/main/java/com/ivanbentes/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package com.ivanbentes.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
