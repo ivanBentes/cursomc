02c136d3319286102c8d484bb31a0ad9f538f58c
Ivan Bentes <ivan.ra.bentes@gmail.com>
2018-05-15 11:19:28 -0400

    REST testado

---
+++ src/main/java/com/ivanbentes/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ivanbentes.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ibentes
+ *
+ */
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+
+}
