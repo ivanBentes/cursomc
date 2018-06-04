cc4df910f89707ed13e3dc2f4aad0b4d2d3f9d7a
Ivan Bentes <ivan.ra.bentes@gmail.com>
2018-06-03 20:25:53 -0400

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ivanbentes/cursomc/services/CategoriaService.java
+++ src/main/java/com/ivanbentes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ivanbentes.cursomc.domain.Categoria;
 import com.ivanbentes.cursomc.repositories.CategoriaRepository;
+import com.ivanbentes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
-		
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
